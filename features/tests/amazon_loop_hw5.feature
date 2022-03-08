7c8a998e804255d5c7a976692729ae9b06eeb1f9
lisaturtle177 <98904528+lisaturtle177@users.noreply.github.com>
2022-03-08 12:04:32 -0800

    hw 5 still questions re loop

---
+++ features/tests/amazon_loop_hw5.feature
@@ -0,0 +1,6 @@
+# Created by reneeherscovici at 3/7/22
+Feature: Tests for product page
+
+  Scenario: User can select colors
+    Given Open product B07BJKRR25 page
+    Then Verify user can click through options
--- sample_script.py
+++ sample_script.py
@@ -1,23 +1,27 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+SEARCH_BTN=(By.NAME, 'btnK')
 
 # init driver
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path='/Users/reneeherscovici/Desktop/Automation/python-selenium-automation/chromedriver')
 driver.maximize_window()
 
 # open the url
 driver.get('https://www.google.com/')
-
 search = driver.find_element(By.NAME, 'q')
 search.clear()
 search.send_keys('Dress')
 
 # wait for 4 sec
-sleep(4)
-
+#sleep(4)
+WebDriverWait(driver, 4).until(EC.element_to_be_clickable(SEARCH_BTN), message='Btn not clickable')
+#driver.wait.until(EC.element_to_be_clickable(SEARCH_BTN), message= 'Btn not clickable')
 # click search
-driver.find_element(By.NAME, 'btnK').click()
+driver.find_element(*SEARCH_BTN).click()
 
 # verify
 assert 'dress' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
